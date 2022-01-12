1b6c96df3244f100c45ec2e66f826c2e8bcf9cfd
SergeevSezar <sergeev.sezar@yandex.ru>
2022-01-13 05:45:28 +0900

    create project

---
+++ app/src/main/java/movieapp/moviek/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package movieapp.moviek.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import movieapp.moviek.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
