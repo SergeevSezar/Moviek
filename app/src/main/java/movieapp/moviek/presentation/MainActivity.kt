5ea1a3ed463beb09831acfe78d4929f0308fbdd4
SergeevSezar <sergeev.sezar@yandex.ru>
2022-01-18 06:16:44 +0900

    added activity movie poster layout
    добавили макет главного экрана

--- app/src/main/java/movieapp/moviek/presentation/MainActivity.kt
+++ app/src/main/java/movieapp/moviek/presentation/MainActivity.kt
@@ -7,6 +7,6 @@ import movieapp.moviek.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_poster)
     }
 }
