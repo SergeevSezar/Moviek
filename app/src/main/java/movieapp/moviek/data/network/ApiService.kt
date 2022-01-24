50a6babad917099f0c33f2bed841244a7d9a48c1
SergeevSezar <sergeev.sezar@yandex.ru>
2022-01-25 06:02:28 +0900

    finished network

--- app/src/main/java/movieapp/moviek/data/network/ApiService.kt
+++ app/src/main/java/movieapp/moviek/data/network/ApiService.kt
@@ -1,23 +1,25 @@
 package movieapp.moviek.data.network
 
+import io.reactivex.rxjava3.core.Single
+import movieapp.moviek.data.network.model.MovieListDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("popular")
-    suspend fun getPopularMovieList(
+    fun getPopularMovieList(
         @Query(QUERY_PARAM_API_KEY) apiKey: String = "485fbc212ab76526edb2699c29b0657d",
         @Query(QUERY_PARAM_LANGUAGE) language: String = "ru-RU",
         @Query(QUERY_PARAM_PAGE) page: Int = 1
-    )
+    ): Single<MovieListDto>
 
     @GET("top_rated")
-    suspend fun getTopRatedMovieList(
+    fun getTopRatedMovieList(
         @Query(QUERY_PARAM_API_KEY) apiKey: String = "485fbc212ab76526edb2699c29b0657d",
         @Query(QUERY_PARAM_LANGUAGE) language: String = "ru-RU",
         @Query(QUERY_PARAM_PAGE) page: Int = 1
-    )
+    ): Single<MovieListDto>
 
     companion object{
         private const val QUERY_PARAM_API_KEY = "api_key"
--- app/src/main/java/movieapp/moviek/presentation/MainActivity.kt
+++ app/src/main/java/movieapp/moviek/presentation/MainActivity.kt
@@ -1,12 +1,34 @@
 package movieapp.moviek.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import movieapp.moviek.R
+import movieapp.moviek.data.network.ApiFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_poster)
+        val disposable = ApiFactory.apiService.getTopRatedMovieList()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST", it.toString())
+            }, {
+                it.message?.let { it1 -> Log.d("TEST", it1) }
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
