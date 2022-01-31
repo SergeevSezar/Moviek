7c9e4e994e6a37317df2ea8a75b140a5ad3d18cf
SergeevSezar <sergeev.sezar@yandex.ru>
2022-02-01 06:48:59 +0900

    added view holder

---
+++ app/src/main/java/movieapp/moviek/presentation/adapter/MovieInfoDiffCallBack.kt
@@ -0,0 +1,14 @@
+package movieapp.moviek.presentation.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import movieapp.moviek.domain.Movie
+
+object MovieInfoDiffCallBack: DiffUtil.ItemCallback<Movie>() {
+    override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+        return oldItem == newItem
+    }
+}
---
+++ app/src/main/java/movieapp/moviek/presentation/adapter/MovieInfoViewHolder.kt
@@ -0,0 +1,7 @@
+package movieapp.moviek.presentation.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import movieapp.moviek.databinding.MoviePosterBinding
+
+class MovieInfoViewHolder(val binding: MoviePosterBinding): RecyclerView.ViewHolder(binding.root) {
+}
